b87d86ca21aa7887119b34ee8f19c19d3849d5c6
syn <vicdemilly@gmail.com>
2019-04-08 16:36:04 +0200

    Premier ajout de condition

---
+++ main.js
@@ -0,0 +1,12 @@
+function majeur()
+{
+    var age = document.getElementById("age").value;
+    if (age >= 18)
+    {
+      alert("Vous êtes majeur");
+    }
+    else
+    {
+      alert("Vous êtes mineur");
+    }
+}
