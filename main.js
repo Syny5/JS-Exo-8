6c61b3a086e23c199f601eecb65d5155ace29022
syn <vicdemilly@gmail.com>
2019-04-11 16:47:59 +0200

    Ajout de commentaire

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 function display()
 {
-    let age = document.getElementById("age").value;
+    let age = document.querySelector('#age').value;
+    // querySelector est une alternative au getElementById
     if (age >= 18)
     {
-      alert("Vous êtes majeur");
-    }
-    else
-    {
-      alert("Vous êtes mineur");
+      alert('Vous êtes majeur');
+    } else {
+      alert('Vous êtes mineur');
     }
 }
 // Ajout de condition : Si l'âge indiqué est supérieur ou égal à 18, alors la boite de dialogue indique "vous êtes majeurs"
-// a l'inverse, elle indiquera "Vous êtes mineur" si l'âge indiqué est inférieur à 18.
+// À l'inverse, elle indiquera "Vous êtes mineur" si l'âge indiqué est inférieur à 18.
