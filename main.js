81fdba0944c28c3c6119e5121d645ce5b2ccd00a
syn <vicdemilly@gmail.com>
2019-04-11 09:46:44 +0200

    Autocorrection et ajout de commentaires

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function majeur()
+function display()
 {
     let age = document.getElementById("age").value;
     if (age >= 18)
@@ -10,3 +10,5 @@ function majeur()
       alert("Vous êtes mineur");
     }
 }
+// Ajout de condition : Si l'âge indiqué est supérieur ou égal à 18, alors la boite de dialogue indique "vous êtes majeurs"
+// a l'inverse, elle indiquera "Vous êtes mineur" si l'âge indiqué est inférieur à 18.
